37babcc369c2487308f388516c0b4f242d64929a
DESKTOP-F3CIMG5\202_18 <s23hee03@naver.com>
2025-07-04 17:26:33 +0900

    오늘은야무지게성공~

---
+++ Assets/GameManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool isGameOver = false;
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverUI;
+
+    private int score = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("씬에 두개 이상의 게임 매니저가 존재합니다!");
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        scoreText.text = "Score: " + score;
+    }
+    public void OnPlayerDead()
+    {
+        isGameOver = true;
+        gameOverUI.SetActive(true);
+    }
+}
